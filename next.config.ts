ef2bf55d9e7a5471ac9fcc5a09955391af5e23e2
keitaMax <86122925+NiheiKeita@users.noreply.github.com>
2025-10-25 14:19:53 +0900

    GitHub Actions: Update artifact upload path to use 'out' directory and configure Next.js settings

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+const repositoryName = "zen-book-nextjs-sample-app";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  ...(isProd
+    ? {
+        basePath: `/${repositoryName}`,
+        assetPrefix: `/${repositoryName}/`,
+      }
+    : {}),
 };
 
 export default nextConfig;
